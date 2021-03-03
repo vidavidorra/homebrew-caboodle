4167e29c9313621d3d9ef4e590998f29fe07d04f
Jeroen de Bruijn <vv9239@gmail.com>
2021-03-03 21:24:41 +0100

    fix(list): require at least one option, validated using Joi

--- src/cli/commands/list.ts
+++ src/cli/commands/list.ts
@@ -1,38 +1,69 @@
-import { Arguments as CliArguments, Command, environment } from '..';
+import * as cli from '..';
+import Joi from 'joi';
 import { logger } from '../../logger';
+import { rfc2119KeyWordsToUpperCase } from '../../helpers';
 
-interface Arguments extends CliArguments {
+interface Arguments extends cli.Arguments {
   existing?: boolean;
   create?: boolean;
   update?: boolean;
 }
 
-const nestedCommandName = 'list';
-const command: Command<CliArguments, Arguments> = {
+const schema = cli.schema
+  .keys({
+    existing: Joi.boolean(),
+    create: Joi.boolean(),
+    update: Joi.boolean(),
+  })
+  .or('existing', 'create', 'update');
+
+const command: cli.Command<cli.Arguments, Arguments> = {
   command: 'list',
   describe: 'List formulae',
   builder: (yargs) => {
-    return yargs.epilogue(environment.argumentSourceMessage()).options({
-      existing: {
-        alias: 'e',
-        describe: 'List existing formulae',
-        type: 'boolean',
-      },
-      create: {
-        alias: 'c',
-        describe: 'List formulae to be created',
-        type: 'boolean',
-      },
-      update: {
-        alias: 'u',
-        describe: 'List formulae to be updated',
-        type: 'boolean',
-      },
-    });
+    return yargs
+      .epilogue(cli.environment.argumentSourceMessage())
+      .options({
+        existing: {
+          alias: 'e',
+          describe: 'List existing formulae',
+          type: 'boolean',
+        },
+        create: {
+          alias: 'c',
+          describe: 'List formulae to be created',
+          type: 'boolean',
+        },
+        update: {
+          alias: 'u',
+          describe: 'List formulae to be updated',
+          type: 'boolean',
+        },
+      })
+      .check((argv) => {
+        logger.debug({ argv }, 'Checking');
+        const validation = schema.validate(argv, {
+          abortEarly: false,
+          allowUnknown: true,
+        });
+
+        if (validation.error !== undefined) {
+          logger.debug('some error');
+          throw new Error(
+            [
+              'Invalid argument(s):',
+              ...validation.error.details.map(
+                (e) => `  ${rfc2119KeyWordsToUpperCase(e.message)}`,
+              ),
+            ].join('\n'),
+          );
+        }
+
+        return true;
+      });
   },
   handler: (args) => {
     logger.info(args, 'CLI list arguments');
-    logger.debug('debug');
   },
 };
 
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,6 +1,6 @@
+import { Arguments, schema } from './arguments';
 import { Environment, environment } from './environment';
-import { Arguments } from './arguments';
 import { Command } from './command';
 import { run } from './cli';
 
-export { Environment, environment, Arguments, Command, run };
+export { Environment, environment, Arguments, schema, Command, run };
